e9315e1a94b8194443a68a53dc13f499428e9bc6
evelin.herrera <evelin.herrera@banistmo.com>
2021-03-12 21:26:52 -0500

    move table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MenuComponent } from './components/menu/menu.component';
-import { TableComponent } from './table/table.component';
 import { HomeComponent } from './modules/home/home.component';
 import { DetailComponent } from './modules/detail/detail.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { TableComponent } from './modules/table/table.component';
 
 @NgModule({
   declarations: [
